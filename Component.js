d9527b7025750c4822fb30afd9d6cb41a9acf7ee
thomashombergerwebde <thomashombergerwebde@users.noreply.github.com>
2020-04-26 10:57:26 +0200

    v4

---
+++ Component.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel, ResourceModel) {
+	"use strict";
+
+	return UIComponent.extend("homberger.weatherapp.Component", {
+		metadata : {
+			manifest: "json"
+		}
+	});
+
+}); 
