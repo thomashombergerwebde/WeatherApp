8e0e5dc32c66ece34fe0a811e12ecb9a6489b728
D019820 <thomas.homberger@sap.com>
2020-05-13 13:04:06 +0200

    formatter simplified

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -35,37 +35,12 @@ sap.ui.define([], function () {
 			return returnValue;
 		},
 
-		temperatureValueColor: function (sValue, sPosition, oModel){
-
-			//oModel is provided when formatter is called by controller in case the
-			//threshold was changed
-			if(!oModel){
-					 oModel = this.getView().getModel("settings");
+		temperatureValueColor: function (sValue, iThreshold){
+			if(sValue<iThreshold){
+				return sap.m.ValueColor.Neutral;  //blue color
+			} else if(sValue>=iThreshold){
+				return sap.m.ValueColor.Error; 	  //red color
 			}
-
- 			if(!sValue||!sPosition){
-				return sap.m.ValueColor.Neutral;
-			}
-
-			//Indoor
-			if(sPosition=="Indoor"){
-
-				if(sValue<oModel.getProperty("/threshold/temperature/indoor/value")){
-					return sap.m.ValueColor.Neutral;
-				} else if(sValue>=oModel.getProperty("/threshold/temperature/indoor/value")){
-					return sap.m.ValueColor.Error;
-				}
-
-			//Outdoor
-			} else if(sPosition=="Outdoor"){
-
-				if(sValue<oModel.getProperty("/threshold/temperature/outdoor/value")){
-					return sap.m.ValueColor.Neutral;
-				} else if(sValue>=oModel.getProperty("/threshold/temperature/outdoor/value")){
-					return sap.m.ValueColor.Error;
-				}
-			}
-
 		},
 
 		humidityValue: function (sValue){
@@ -77,53 +52,21 @@ sap.ui.define([], function () {
 			}
 		},
 
-		humidityValueColor: function (sValue, sPosition, oModel){
+		humidityValueColor: function (sValue, iThresholdLow, iThresholdHigh){
 
-			//oModel is provided when formatter is called by controller in case the
-			//threshold was changed
-			if(!oModel){
-				 oModel = this.getView().getModel("settings");
-			}
-
- 			if(!sValue||!sPosition){
+			if(sValue<=0){
 				return sap.m.ValueColor.Neutral;
+			} else if(sValue < iThresholdLow-5) {   //red color
+				return sap.m.ValueColor.Error;
+			} else if(sValue < iThresholdLow) {     //yellow color
+				return sap.m.ValueColor.Critical;
+			} else if(sValue < iThresholdHigh) {    //green color
+				return sap.m.ValueColor.Good;
+			} else if(sValue < iThresholdHigh+5) {  //yellow color
+				return sap.m.ValueColor.Critical;
+			} else if(sValue >= iThresholdHigh+5) { //red color
+				return sap.m.ValueColor.Error
 			}
-
-			//Indoor
-			if(sPosition=="Indoor"){
-
-				if(sValue<=0){
-					return sap.m.ValueColor.Neutral;
-				} else if(sValue < (oModel.getProperty("/threshold/humidity/indoor/range/0")-5)){
-					return sap.m.ValueColor.Error
-				} else if(sValue < oModel.getProperty("/threshold/humidity/indoor/range/0")){
-					return sap.m.ValueColor.Critical;
-				} else if(sValue < oModel.getProperty("/threshold/humidity/indoor/range/1")){
-					return sap.m.ValueColor.Good;
-				} else if(sValue < (oModel.getProperty("/threshold/humidity/indoor/range/1")+5)){
-					return sap.m.ValueColor.Critical;
-				} else if(sValue >= (oModel.getProperty("/threshold/humidity/indoor/range/1")+5)){
-					return sap.m.ValueColor.Error
-				}
-
-			//Outdoor
-			} else if(sPosition=="Outdoor"){
-
-				if(sValue<=0){
-					return sap.m.ValueColor.Neutral;
-				} else if(sValue < (oModel.getProperty("/threshold/humidity/outdoor/range/0")-5)){
-					return sap.m.ValueColor.Error
-				} else if(sValue < oModel.getProperty("/threshold/humidity/outdoor/range/0")){
-					return sap.m.ValueColor.Critical;
-				} else if(sValue < oModel.getProperty("/threshold/humidity/outdoor/range/1")){
-					return sap.m.ValueColor.Good;
-				} else if(sValue < (oModel.getProperty("/threshold/humidity/outdoor/range/1")+5)){
-					return sap.m.ValueColor.Critical;
-				} else if(sValue >= (oModel.getProperty("/threshold/humidity/outdoor/range/1")+5)){
-					return sap.m.ValueColor.Error
-				}
-			}
-
 		},
 
 		temperatureIndicator: function (sOldValue, sNewValue){
