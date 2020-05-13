ca7e368af0eb5eed508b3c5fe053add39027e556
D019820 <thomas.homberger@sap.com>
2020-05-13 16:52:25 +0200

    settings view & controller corrected

--- webapp/controller/settings.controller.js
+++ webapp/controller/settings.controller.js
@@ -19,9 +19,8 @@ sap.ui.define(
 			}
 		},
 
-		/*
 		onSliderMovePictureCarousel: function (oEvent) {
-
+			/*
 			var that = this;
 
 			//Move picture carousel forward every n seconds
@@ -30,12 +29,17 @@ sap.ui.define(
 				var car = that.getView().byId("carousel");
 				car.next();
 			}, this.getView().getModel("settings").getProperty("/show/picture/value") * 1000);
-
+            */
+			var settings = localStorage.getItem("settings");
+			var oSettings = JSON.parse(settings);
+			oSettings.show.picture.value = oEvent.getParameter("value");
+			settings = JSON.stringify(oSettings);
+			localStorage.setItem("settings", settings);
 		},
 
 		onSliderMoveChartCarousel: function (oEvent) {
-
-			var that = this;
+			/*
+		 	var that = this;
 
 			//Move chart carousel forward every n seconds
 			clearInterval(this.moveChartCarousel);
@@ -43,9 +47,46 @@ sap.ui.define(
 				var car = that.getView().byId("carouselHistory");
 				car.next();
 			}, this.getView().getModel("settings").getProperty("/show/chart/value") * 1000);
+		 	*/
+			var settings = localStorage.getItem("settings");
+			var oSettings = JSON.parse(settings);
+			oSettings.show.chart.value = oEvent.getParameter("value");
+			settings = JSON.stringify(oSettings);
+			localStorage.setItem("settings", settings);
+		},
 
+		onSliderChangeTemperatureIndoorColor: function (oEvent) {
+			var settings = localStorage.getItem("settings");
+			var oSettings = JSON.parse(settings);
+			oSettings.threshold.temperature.indoor.value = oEvent.getParameter("value");
+			settings = JSON.stringify(oSettings);
+			localStorage.setItem("settings", settings);
+		},
+
+		onSliderChangeTemperatureOutdoorColor: function (oEvent) {
+			var settings = localStorage.getItem("settings");
+			var oSettings = JSON.parse(settings);
+			oSettings.threshold.temperature.outdoor.value = oEvent.getParameter("value");
+			settings = JSON.stringify(oSettings);
+			localStorage.setItem("settings", settings);
+		},
+
+		onSliderChangeHumidityIndoorColor: function(oEvent) {
+			var settings = localStorage.getItem("settings");
+			var oSettings = JSON.parse(settings);
+			oSettings.threshold.humidity.indoor.range[0] = oEvent.getParameter("range")[0];
+			oSettings.threshold.humidity.indoor.range[1] = oEvent.getParameter("range")[1];
+			settings = JSON.stringify(oSettings);
+			localStorage.setItem("settings", settings);
+		},
+
+		onSliderChangeHumidityOutdoorColor: function(oEvent) {
+			var settings = localStorage.getItem("settings");
+			var oSettings = JSON.parse(settings);
+			oSettings.threshold.humidity.outdoor.range[0] = oEvent.getParameter("range")[0];
+			oSettings.threshold.humidity.outdoor.range[1] = oEvent.getParameter("range")[1];
+			settings = JSON.stringify(oSettings);
+			localStorage.setItem("settings", settings);
 		}
-		*/
 	});
-
 });
